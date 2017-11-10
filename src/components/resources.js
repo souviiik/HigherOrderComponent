8c87933c6cb2ee34853a76042bcc323a2a0c34ae
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2017-11-10 15:25:07 +0530

    Completed HOC implementation
    Completed HOC implementation.

--- src/components/resources.js
+++ src/components/resources.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default () => {
     return (
         <div>
-            <h1>Secret Resources</h1>
+            <h1>Super Special Secret Recipe</h1>
+            <ul>
+                <li>1 cup sugar</li>
+                <li>1 cup pepper</li>
+                <li>1 cup salt</li>
+            </ul>
         </div>
     )
 }
